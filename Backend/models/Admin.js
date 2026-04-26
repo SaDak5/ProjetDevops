931fa169f6f109f1ff6b1eb379ec0b1c0cc3c29f
SaDak5 <mghirbiaziz047@gmail.com>
2026-04-26 01:07:59 +0100

    modification backend

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('./User');
 
-// On utilise le même schéma que User pour Admin
+// On utilise le même schéma que User pour le Admin
 const Admin = mongoose.model('Admin', User.schema);
 module.exports = Admin;
